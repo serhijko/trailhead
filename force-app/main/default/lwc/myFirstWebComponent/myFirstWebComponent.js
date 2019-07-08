2ffd73025c18c1ef91f2e55c9bf7afac733177da
serhijko <serhijko>
2019-07-08 22:41:04 +0300

    adding Code and Metadate to first lwc

--- force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
+++ force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class MyFirstWebComponent extends LightningElement {}
+export default class HelloIteration extends LightningElement {
+    @track
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Amy Tailor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+    ];
+}
